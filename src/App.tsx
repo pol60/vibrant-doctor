db55c1ca9b7b8ed8677bfdaa6ee75b3b0bbdd743
niko <pol60g54@gmail.com>
2025-06-06 14:46:09 +0300

    5

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "./context/LanguageContext";
 import { motion, AnimatePresence } from "framer-motion";
 import Header from "./components/Header";
@@ -23,7 +22,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <div className="flex flex-col min-h-screen">
             <Header />
             <main className="flex-grow pt-16">
@@ -47,7 +46,7 @@ const App = () => (
             </main>
             <Footer />
           </div>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </LanguageProvider>
   </QueryClientProvider>
