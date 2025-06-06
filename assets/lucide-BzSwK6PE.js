533aa90aba41b5348b057df33f069caed95c1627
niko <pol60g54@gmail.com>
2025-06-06 14:50:57 +0300

    Updates

--- assets/lucide-BzSwK6PE.js
+++ assets/lucide-BzSwK6PE.js
@@ -1,4 +1,4 @@
-import{r as o}from"./react-Bs7DjbeO.js";/**
+import{r as o}from"./react-BoaLSWgV.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
